9591098aacb5d0e70cfc373e0d27857294310a15
Abdallah <abdallahmnas@gmail.com>
2025-07-25 11:54:53 +0100

    Cleaup before final push

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
 const app = express();
-import sequelize from "./models";
 import cors from "cors";
 import errorHandler from "./middlewares/handlers/error";
 import Config from "./utils/config";
@@ -13,7 +12,6 @@ app.use(expressfile());
 app.use(cors());
 app.use(express.json()); //{ limit: "50mb" }
 app.use(express.urlencoded({ extended: true })); //{ extended: true, limit: "50mb" }
-// app.use(morgan("dev"));
 
 app.use((req, res, next) => {
   Log.success(
@@ -26,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to QuickFIll Service");
+  res.send("Welcome to Raven Service");
 });
 
 app.get("/health", async (req: Request, res: Response) => {
@@ -34,27 +32,8 @@ app.get("/health", async (req: Request, res: Response) => {
   data.date = new Date();
 
   try {
-    // Check Sequelize database connection
-    // await dbClient.authenticate();//Old
-    await sequelize.authenticate();
-    data.database = "Ok";
-  } catch (error) {
-    data.database = "Error";
-  }
-
-  try {
-    // Check Redis connection
-    // await redisClient.ping();
-    // data.redis = "Ok";
-  } catch (error) {
-    data.redis = "Error";
-  }
-
-  try {
     data.uptime = process.uptime();
     data.server = "Ok";
-    // data.sequelize = "Connection to Sequelize database is healthy";
-    // data.redis = "Connection to Redis is healthy";
 
     res.status(200).send(data);
   } catch (error: any) {
@@ -64,8 +43,6 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 import routesV1 from "./routes/v1";
 import Log from "./utils/loggers";
-import knex from "./db/knex_db";
-import userRepository from "./repositories/user.repository";
 
 app.use(`/v1`, routesV1);
 
@@ -81,16 +58,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 // attach error handler middleware
 app.use(errorHandler);
 
-const syncOptions =
-  Config.environment !== "production" ? { alter: true } : { alter: true };
-
 app.listen(port, () => {
   // redis c;
   Log.info("Server is running on port " + port);
 });
-// sequelize.sync(syncOptions).then(() => {
-//   app.listen(port, () => {
-//     // redis c;
-//     Log.info("Server is running on port " + port);
-//   });
-// });
